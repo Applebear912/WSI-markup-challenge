7ec92919f80565b6c86f1010fc33896f83ccaa11
Pingping Xiao <ppx2021@gmail.com>
2021-12-05 21:59:45 -0600

    updated modal style

--- src/AddToCartModal.js
+++ src/AddToCartModal.js
@@ -3,16 +3,17 @@ import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import Modal from "@mui/material/Modal";
+import CloseIcon from "@mui/icons-material/Close";
+import Grid from "@mui/material/Grid";
 
 const style = {
   position: "absolute",
   top: "50%",
   left: "50%",
-  transform: "translate(-50%, -50%)",
+  transform: "translate(-30%, -30%)",
   height: 300,
   width: 500,
   bgcolor: "background.paper",
-  border: "2px #000",
   boxShadow: 24,
   p: 4,
 };
@@ -38,10 +39,29 @@ export default function AddToCartModal() {
         aria-describedby="modal-modal-description"
       >
         <Box sx={style}>
-          <Typography id="modal-modal-title" variant="h6" component="h2">
-            Added to cart!
-          </Typography>
-          <Button type="submit">Checkout Now</Button>
+          <CloseIcon
+            style={{
+              display: "flex",
+              flexDirection: "row-reverse",
+              p: 1,
+              m: 1,
+              float: "right",
+            }}
+          />
+          <Grid sx={12} className="modalText">
+            <Typography id="modal-modal-title" variant="h6" component="h2">
+              Added to cart!
+            </Typography>
+            <Button
+              variant="contained"
+              style={{
+                p: 20,
+                m: 20,
+              }}
+            >
+              Checkout Now
+            </Button>
+          </Grid>
         </Box>
       </Modal>
     </div>
