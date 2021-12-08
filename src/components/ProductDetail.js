96239e45b07fbc1921a4178b35b07a098e54ef58
Pingping Xiao <ppx2021@gmail.com>
2021-12-07 18:09:48 -0600

    added controlled rating stars

--- src/components/ProductDetail.js
+++ src/components/ProductDetail.js
@@ -3,6 +3,9 @@ import StarIcon from "@mui/icons-material/Star";
 import { makeStyles } from "@mui/styles";
 import TextField from "@mui/material/TextField";
 import Divider from "@mui/material/Divider";
+import Rating from "@mui/material/Rating";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
 
 import Accordion from "./Accordion.js";
 import AddToCartModal from "./AddToCartModal.js";
@@ -18,31 +21,26 @@ const useStyles = makeStyles(() => ({
 
 export default function ProductDetail() {
   const classes = useStyles();
+  const [value, setValue] = React.useState(5);
 
   return (
     <div className="productDetail">
       <h2>Raised Garden Bed 48x30x24 Inch - Elevated Wooden Planter</h2>
       <h5>ONLY AT WILLIAMS SONOMA</h5>
-      <StarIcon
-        className={classes.hover1}
-        style={{ justifyContent: "flex-start", fontSize: 30 }}
-      />
-      <StarIcon
-        className={classes.hover1}
-        style={{ justifyContent: "flex-start", fontSize: 30 }}
-      />
-      <StarIcon
-        className={classes.hover1}
-        style={{ justifyContent: "flex-start", fontSize: 30 }}
-      />
-      <StarIcon
-        className={classes.hover1}
-        style={{ justifyContent: "flex-start", fontSize: 30 }}
-      />
-      <StarIcon
-        className={classes.hover1}
-        style={{ justifyContent: "flex-start", fontSize: 30 }}
-      />
+      <Box
+        sx={{
+          "& > legend": { mt: 2 },
+        }}
+      >
+        <Typography component="legend"></Typography>
+        <Rating
+          name="simple-controlled"
+          value={value}
+          onChange={(event, newValue) => {
+            setValue(newValue);
+          }}
+        />
+      </Box>
       <p>
         Rustic and natural style planter bed has 8.7" depth, provides enough
         depth for plants and vegetables to breathe, grow healthily and also less
